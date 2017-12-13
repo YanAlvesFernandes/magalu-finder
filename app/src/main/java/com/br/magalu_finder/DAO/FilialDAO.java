20570f0c01d5e2369248b9fa3b72d88856759c09
Yan Alves <yan-alves@hotmail.com>
2017-12-13 20:49:52 -0200

    Incluso formulrio de cadastro da filial

---
+++ app/src/main/java/com/br/magalu_finder/DAO/FilialDAO.java
@@ -0,0 +1,92 @@
+package com.br.magalu_finder.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+
+import com.br.magalu_finder.Model.Filial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Yan Alves on 13/12/2017.
+ */
+
+public class FilialDAO extends SQLiteOpenHelper {
+
+    //Nome do banco e versão que deve ser incrementada após alterações do BD
+    public FilialDAO(Context context) {
+        super(context, "magalu_finder", null, 1);
+    }
+
+    //Criação da tabela Localizacao
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Filial (id INTEGER PRIMARY KEY, desc_filial TEXT NOT NULL, cep TEXT NOT NULL);";
+        db.execSQL(sql);
+    }
+
+    //Método chamado sempre que houver alguma alteração no banco
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS Localizacao");
+        onCreate(db);
+
+    }
+
+    //Método responsável pela inserção no banco de dados
+    public void insere(Filial filial) {
+
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues dados = pegaDadosLocalizacao(filial);
+
+
+        db.insert("Filial", null, dados);
+
+    }
+
+    //Criação do ContentValues que serão inseridos no banco
+    @NonNull
+    private ContentValues pegaDadosLocalizacao(Filial filial) {
+
+        ContentValues dados = new ContentValues();
+        dados.put("desc_filial", filial.getDesc_filial().toUpperCase());
+        dados.put("cep", filial.getCep().toUpperCase());
+        return dados;
+    }
+
+
+    //Busca no banco
+    public List<Filial> buscaFilial() {
+        String sql = "SELECT * FROM Filial";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery(sql, null);
+
+        List<Filial>filiais = new ArrayList<Filial>();
+        while (c.moveToNext()){
+
+            Filial filial = new Filial();
+            filial.setId(c.getLong(c.getColumnIndex("id")));
+            filial.setDesc_filial(c.getString(c.getColumnIndex("desc_filial")));
+            filial.setCep(c.getString(c.getColumnIndex("cep")));
+            filiais.add(filial);
+        }
+        c.close();
+
+        return filiais;
+    }
+
+    //Função responsável por deletar ítens do banco após busca po ID
+    public void deleta(Filial localizacao) {
+
+        SQLiteDatabase db  = getWritableDatabase();
+        String[] params = {localizacao.getId().toString()};
+        db.delete("Filial", "id = ?", params);
+
+    }
+}
---
+++ app/src/main/java/com/br/magalu_finder/Model/Filial.java
@@ -0,0 +1,36 @@
+package com.br.magalu_finder.Model;
+
+/**
+ * Created by Yan Alves on 13/12/2017.
+ * Criação do objeto Filial
+ */
+
+public class Filial {
+
+    private Long id;
+    private String desc_filial, cep;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesc_filial() {
+        return desc_filial;
+    }
+
+    public void setDesc_filial(String desc_filial) {
+        this.desc_filial = desc_filial;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
