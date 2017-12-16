f887d6337fc4c8cc8c54dc031283fe9636dc2265
Yan Alves <yan-alves@hotmail.com>
2017-12-16 20:50:44 -0200

    Implementação do cadastro de produtos

--- app/src/main/java/com/br/magalu_finder/FormularioProdutos.java
+++ app/src/main/java/com/br/magalu_finder/FormularioProdutos.java
@@ -1,13 +1,91 @@
 package com.br.magalu_finder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.br.magalu_finder.DAO.ProdutoDAO;
+import com.br.magalu_finder.Model.Produto;
+
+import java.io.File;
 
 public class FormularioProdutos extends AppCompatActivity {
+
+    public static final int CODIGO_CAMERA = 567;
+    private ProdutoHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_produtos);
+
+        //Inicializção do helper
+        helper = new ProdutoHelper(this);
+
+        Button botaoFoto = (Button)findViewById(R.id.btnFotoProduto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
+            }
+        });
+
+        //Botão reponsável para ir a listagem de filiais cadastradas
+        Button btnIrFiliais = (Button) findViewById(R.id.btnIrFiliais);
+        btnIrFiliais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent voltarFiliais = new Intent(FormularioProdutos.this, MainActivity.class);
+                startActivity(voltarFiliais);
+            }
+        });
+
+        //Botão responsável por efetuar o cadastro
+        Button btnCadastrarFilial = (Button) findViewById(R.id.btnCadastrarFilial);
+        btnCadastrarFilial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Se obtendo os dados cadastrados nos TextViews
+                Produto produto = helper.pegaProduto();
+                //Criação do objeto para acesso ao bando de dados
+                ProdutoDAO dao = new ProdutoDAO(FormularioProdutos.this);
+                //Inserindo os dasos e confirmação da inserção
+                dao.insere(produto);
+                Toast.makeText(FormularioProdutos.this, "Produto cadastrado com sucesso! " + produto.getDesc_produto(), Toast.LENGTH_SHORT).show();
+
+                dao.close();
+                finish();
+
+                Intent voltarFiliais = new Intent(FormularioProdutos.this, MainActivity.class);
+                startActivity(voltarFiliais);
+
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == CODIGO_CAMERA) {
+                //Abrir a foto
+                helper.carregaImagem(caminhoFoto);
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/br/magalu_finder/ProdutoHelper.java
@@ -0,0 +1,53 @@
+package com.br.magalu_finder;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.br.magalu_finder.Model.Produto;
+
+/**
+ * Created by Yan Alves on 16/12/2017.
+ */
+
+public class ProdutoHelper {
+
+    private final EditText desc_produto;
+    private final EditText valor_produto;
+    private final ImageView campoFoto;
+
+    private Produto produto;
+
+    public ProdutoHelper(FormularioProdutos activity){
+
+        desc_produto = (EditText)activity.findViewById(R.id.txtDescProduto);
+        valor_produto = (EditText) activity.findViewById(R.id.txtValorProduto);
+        campoFoto = (ImageView) activity.findViewById(R.id.formulario_foto);
+
+        produto = new Produto();
+
+    }
+
+    public Produto pegaProduto() {
+
+        produto.setDesc_produto(desc_produto.getText().toString());
+        produto.setValor_produto(Double.valueOf(String.valueOf(valor_produto.getText())));
+        produto.setCaminhoFoto((String) campoFoto.getTag());
+        return produto;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+
+        if (caminhoFoto != null) {
+
+            //Abrir a foto
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = bitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);
+        }
+    }
+
+}
